2090adcc600b7b75b7e2e67173dc7d7b4704973a
UDIN-K <safrisam.id09@gmail.com>
2026-04-23 23:01:19 +0700

    update

--- lib/dataconnect_generated/delete_review.dart
+++ lib/dataconnect_generated/delete_review.dart
@@ -4,16 +4,24 @@ class DeleteReviewVariablesBuilder {
   String movieId;
 
   final FirebaseDataConnect _dataConnect;
-  DeleteReviewVariablesBuilder(this._dataConnect, {required  this.movieId,});
-  Deserializer<DeleteReviewData> dataDeserializer = (dynamic json)  => DeleteReviewData.fromJson(jsonDecode(json));
-  Serializer<DeleteReviewVariables> varsSerializer = (DeleteReviewVariables vars) => jsonEncode(vars.toJson());
+  DeleteReviewVariablesBuilder(
+    this._dataConnect, {
+    required this.movieId,
+  });
+  Deserializer<DeleteReviewData> dataDeserializer =
+      (dynamic json) => DeleteReviewData.fromJson(jsonDecode(json));
+  Serializer<DeleteReviewVariables> varsSerializer =
+      (DeleteReviewVariables vars) => jsonEncode(vars.toJson());
   Future<OperationResult<DeleteReviewData, DeleteReviewVariables>> execute() {
     return ref().execute();
   }
 
   MutationRef<DeleteReviewData, DeleteReviewVariables> ref() {
-    DeleteReviewVariables vars= DeleteReviewVariables(movieId: movieId,);
-    return _dataConnect.mutation("DeleteReview", dataDeserializer, varsSerializer, vars);
+    DeleteReviewVariables vars = DeleteReviewVariables(
+      movieId: movieId,
+    );
+    return _dataConnect.mutation(
+        "DeleteReview", dataDeserializer, varsSerializer, vars);
   }
 }
 
@@ -21,27 +29,25 @@ class DeleteReviewVariablesBuilder {
 class DeleteReviewReviewDelete {
   final String userId;
   final String movieId;
-  DeleteReviewReviewDelete.fromJson(dynamic json):
-  
-  userId = nativeFromJson<String>(json['userId']),
-  movieId = nativeFromJson<String>(json['movieId']);
+  DeleteReviewReviewDelete.fromJson(dynamic json)
+      : userId = nativeFromJson<String>(json['userId']),
+        movieId = nativeFromJson<String>(json['movieId']);
   @override
   bool operator ==(Object other) {
-    if(identical(this, other)) {
+    if (identical(this, other)) {
       return true;
     }
-    if(other.runtimeType != runtimeType) {
+    if (other.runtimeType != runtimeType) {
       return false;
     }
 
-    final DeleteReviewReviewDelete otherTyped = other as DeleteReviewReviewDelete;
-    return userId == otherTyped.userId && 
-    movieId == otherTyped.movieId;
-    
+    final DeleteReviewReviewDelete otherTyped =
+        other as DeleteReviewReviewDelete;
+    return userId == otherTyped.userId && movieId == otherTyped.movieId;
   }
+
   @override
   int get hashCode => Object.hashAll([userId.hashCode, movieId.hashCode]);
-  
 
   Map<String, dynamic> toJson() {
     Map<String, dynamic> json = {};
@@ -50,7 +56,7 @@ class DeleteReviewReviewDelete {
     return json;
   }
 
-  DeleteReviewReviewDelete({
+  const DeleteReviewReviewDelete({
     required this.userId,
     required this.movieId,
   });
@@ -59,25 +65,25 @@ class DeleteReviewReviewDelete {
 @immutable
 class DeleteReviewData {
   final DeleteReviewReviewDelete? review_delete;
-  DeleteReviewData.fromJson(dynamic json):
-  
-  review_delete = json['review_delete'] == null ? null : DeleteReviewReviewDelete.fromJson(json['review_delete']);
+  DeleteReviewData.fromJson(dynamic json)
+      : review_delete = json['review_delete'] == null
+            ? null
+            : DeleteReviewReviewDelete.fromJson(json['review_delete']);
   @override
   bool operator ==(Object other) {
-    if(identical(this, other)) {
+    if (identical(this, other)) {
       return true;
     }
-    if(other.runtimeType != runtimeType) {
+    if (other.runtimeType != runtimeType) {
       return false;
     }
 
     final DeleteReviewData otherTyped = other as DeleteReviewData;
     return review_delete == otherTyped.review_delete;
-    
   }
+
   @override
   int get hashCode => review_delete.hashCode;
-  
 
   Map<String, dynamic> toJson() {
     Map<String, dynamic> json = {};
@@ -87,7 +93,7 @@ class DeleteReviewData {
     return json;
   }
 
-  DeleteReviewData({
+  const DeleteReviewData({
     this.review_delete,
   });
 }
@@ -95,26 +101,25 @@ class DeleteReviewData {
 @immutable
 class DeleteReviewVariables {
   final String movieId;
-  @Deprecated('fromJson is deprecated for Variable classes as they are no longer required for deserialization.')
-  DeleteReviewVariables.fromJson(Map<String, dynamic> json):
-  
-  movieId = nativeFromJson<String>(json['movieId']);
+  @Deprecated(
+      'fromJson is deprecated for Variable classes as they are no longer required for deserialization.')
+  DeleteReviewVariables.fromJson(Map<String, dynamic> json)
+      : movieId = nativeFromJson<String>(json['movieId']);
   @override
   bool operator ==(Object other) {
-    if(identical(this, other)) {
+    if (identical(this, other)) {
       return true;
     }
-    if(other.runtimeType != runtimeType) {
+    if (other.runtimeType != runtimeType) {
       return false;
     }
 
     final DeleteReviewVariables otherTyped = other as DeleteReviewVariables;
     return movieId == otherTyped.movieId;
-    
   }
+
   @override
   int get hashCode => movieId.hashCode;
-  
 
   Map<String, dynamic> toJson() {
     Map<String, dynamic> json = {};
@@ -122,8 +127,7 @@ class DeleteReviewVariables {
     return json;
   }
 
-  DeleteReviewVariables({
+  const DeleteReviewVariables({
     required this.movieId,
   });
 }
-
